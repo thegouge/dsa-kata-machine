b0fb0aa71e890fdaebae7028c3d06f0cbaaaee4e
thegouge <thegougedev@gmail.com>
2024-07-18 13:13:30 -0700

    all the work I did in Greece

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,35 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(graph: WeightedAdjacencyList, curr: number, needle: number, seen: boolean[], path: number[]): boolean {
+    if (curr === needle) {
+        return true
+    }
 
+    if (seen[curr]) {
+        return false
+    }
+
+    path.push(curr)
+    seen[curr] = true
+
+    for (let i = 0; i < graph[curr].length; i++) {
+        if(walk(graph, graph[curr][i].to, needle, seen, path)) {
+            return true
+        }
+    }
+
+    path.pop()
+
+    return false
+}
+
+export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+    const seen = new Array(graph.length).fill(false)
+    const path: number[] = []
+
+    walk(graph, source, needle, seen, path)
+
+    if (path.length === 0) {
+        return null
+    }
+
+    return [ ...path, needle ]
 }
--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,55 @@
+type Node<T> = {
+    value: T
+    next?: Node<T>
+}
+
 export default class Queue<T> {
     public length: number;
 
-    
+    private head?: Node<T>
+    private tail?: Node<T>
 
     constructor() {
+        this.length = 0
+        this.head = this.tail = undefined
     }
 
     enqueue(item: T): void {
-
-}
+        const newNode = { value: item } as Node<T>
+        this.length++
+        if (!this.tail) {
+            this.head = this.tail = newNode
+            return
+        }
+        this.tail.next = newNode
+        this.tail = newNode
+    }
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined
+        }
+        this.length--
+        const oldHead = this.head
+        if (this.length !== 0) {
+            this.head = this.head.next
+            oldHead.next = undefined
+        } else {
+            this.head = this.tail = undefined
+        }
 
-}
+        return oldHead.value
+    }
     peek(): T | undefined {
-
-}
+        this.debug()
+        return this.head?.value
+    }
+    private debug() {
+        let out = "head"
+        let curr = this.head
+        for (let i = 0; i < this.length; i++) {
+            out += ` -> ${curr?.value}`
+        }
+        out += " <- tail"
+        console.log(out)
+    }
 }
